b681bc14f086826b1ff926aa0294c90912cfe387
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2018-07-02 18:40:53 +0200

    added controller

---
+++ sessions/session2/DapperDemo.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace DapperDemo.WebApi.Controllers
+{
+    [RoutePrefix("products")]
+    public class ProductsController : ApiController
+    {
+        string GetConnectionString() => ConfigurationManager.ConnectionStrings["ProductsDb"].ConnectionString;
+
+        public async Task<IHttpActionResult> GetAllProducts()
+        {
+            using (var connection = new SqlConnection(GetConnectionString()))
+            {
+
+            }
+        }
+    }
+}
