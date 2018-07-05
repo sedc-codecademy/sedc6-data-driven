772dddf8db57c7ea71ac167eaf8fe3960aef2a81
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2018-07-05 18:34:01 +0200

    added common models and DL contracts

---
+++ sessions/session4/MM.Common.Models/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace MM.Common.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int GenreId { get; set; }
+        public int ArtistId { get; set; }
+    }
+}
---
+++ sessions/session4/MM.DataLayer.Contracts/IAlbumRepository.cs
@@ -0,0 +1,14 @@
+﻿using MM.Common.Models;
+using System.Collections.Generic;
+
+namespace MM.DataLayer.Contracts
+{
+    public interface IAlbumRepository
+    {
+        IEnumerable<Album> GetAll(int skip = 0, int take = 100);
+        IEnumerable<Album> GetById(int id);
+        Album Create(Album album);
+        Album Update(Album album);
+        bool Delete(Album album);
+    }
+}
