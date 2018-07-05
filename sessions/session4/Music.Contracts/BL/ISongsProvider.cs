74dfae2d91af0fd6ac883a7906fde26aa614ebbb
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2018-07-05 17:50:41 +0200

    removed session 4

--- sessions/session4/Music.Contracts/BL/ISongsProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using domain = Music.Domain.Models;
-
-namespace Music.DataLayer.Contracts.BL
-{
-    public interface ISongsProvider : ISongsRepository
-    {
-        domain.Album GetAlbumBySong(int songId);
-    }
-}
--- sessions/session4/Music.WebApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Music.WebApp.WebApiApplication" Language="C#" %>
