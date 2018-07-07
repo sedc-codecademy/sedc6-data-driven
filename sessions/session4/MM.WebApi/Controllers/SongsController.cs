9322c773381cd41f45a9e7845d25d0661ab48094
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2018-07-07 13:52:07 +0200

    added songs with album in controller

---
+++ sessions/session4/MM.WebApi/Controllers/SongsController.cs
@@ -0,0 +1,28 @@
+﻿using MM.BusinessLayer.Contracts;
+using MM.BusinessLayer.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MM.WebApi.Controllers
+{
+    [RoutePrefix("songs")]
+    public class SongsController: ApiController
+    {
+        private readonly ISongsProvider provider;
+
+        public SongsController()
+        {
+            provider = new SongsProvider();
+        }
+
+        [Route("~/songs-with-album")]
+        public IHttpActionResult GetSongsWithAlbum(int skip = 0, int take = 100)
+        {
+            var results = provider.GetSongsWithAlbums(skip, take);
+            return Ok(results);
+        }
+    }
+}
